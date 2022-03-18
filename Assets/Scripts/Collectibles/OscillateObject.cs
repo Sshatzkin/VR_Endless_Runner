54d1cad4bdc8e0aa72a0211530c34be79b31f634
Arresh Amleshi <hci@uchicago.edu>
2022-03-18 15:50:44 -0500

    animation for powerups

---
+++ Assets/Scripts/Collectibles/OscillateObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OscillateObject : MonoBehaviour
+{
+    public float oscillationSpeed = 1.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        float y = Mathf.PingPong(Time.time * oscillationSpeed, 1) / 2 + 1.5f; // values that look good in animation
+        transform.position = new Vector3(pos.x, y, pos.z);
+    }
+}
